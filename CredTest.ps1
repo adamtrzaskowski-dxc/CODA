8f0f6925b34a65023d480d247b633bb71ed1f966
Adam Trzaskowski <adam.trzaskowski@dxc.com>
2022-03-04 18:26:26 +0100

    debug

--- CredTest.ps1
+++ CredTest.ps1
@@ -7,4 +7,14 @@ function TEST{
         $cred.Credential_Id
         $map.add($cred.Credential_Id, @($cred.LoginId,$cred.Password))
     }
+Write-Output "MAP WITH SECRET DATA"
+    $map
+    
+    $creds = New-Object System.Collections.ArrayList
+    foreach($item in $map){
+        $credObject = New-Object System.Management.Automation.PSCredential ($userName, (ConvertTo-SecureString $userPassword -AsPlainText -Force))
+        $creds.Add($credObject) > $null
+    }
+Write-Output "ARRAYLIST WITH PSCREDENTIALS"
+    $creds
 }
