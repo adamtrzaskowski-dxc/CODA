dbd479ef4862cae4e1b53b7bfdc585c5951eaeb1
Adam Trzaskowski <adam.trzaskowski@dxc.com>
2022-03-14 21:07:04 +0100

    add new pipeline

---
+++ pipelinetriggeringfreestyle.groovy
@@ -0,0 +1,17 @@
+pipeline {
+    agent any
+    stages {
+        stage('first') {
+            steps {
+                    powershell '''
+                    write-output "scripts to run placeholder"
+                    '''
+            }
+        }
+    }
+    post { 
+        always { 
+            build job: 'CODACreds'
+        }
+    }
+}
